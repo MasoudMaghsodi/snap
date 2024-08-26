fad5db5257f7dde6d8f0a5fffc3a6d33ffc3d097
MasoudMaghsodi <magsodi333@gmail.com>
2024-08-26 20:45:21 +0330

    add states

---
+++ lib/constant/widget_state.dart
@@ -0,0 +1,6 @@
+class CurrentWidgetState {
+  CurrentWidgetState._();
+  static const stateSelectOrgin = 0;
+  static const stateSelectDestination = 1;
+  static const stateRequestDriver = 2;
+}
--- lib/screen/map_screen.dart
+++ lib/screen/map_screen.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_osm_plugin/flutter_osm_plugin.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:geocoding/geocoding.dart';
 import 'package:snap/constant/dimens.dart';
 import 'package:snap/constant/text_styles.dart';
+import 'package:snap/constant/widget_state.dart';
 import 'package:snap/widget/back_button.dart';
+import 'package:snap/gen/assets.gen.dart';
 
 class MapScreen extends StatefulWidget {
   const MapScreen({super.key});
@@ -11,6 +16,20 @@ class MapScreen extends StatefulWidget {
 }
 
 class _MapScreenState extends State<MapScreen> {
+  List<GeocodingPlatform> geoPoints = [];
+  List currentWidgetList = [CurrentWidgetState.stateSelectOrgin];
+  Widget markerIcon = SvgPicture.asset(
+    Assets.icons.origin.path,
+    height: 100,
+    width: 40,
+  );
+  MapController mapController = MapController(
+    initMapWithUserPosition: const UserTrackingOption(
+      unFollowUser: false,
+      enableTracking: false,
+    ),
+    initPosition: GeoPoint(latitude: 36.968797, longitude: 46.090941),
+  );
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -22,25 +41,96 @@ class _MapScreenState extends State<MapScreen> {
               color: Colors.blueAccent.shade200,
             ),
             // current widget
-            Positioned(
-              bottom: 0,
-              left: 0,
-              right: 0,
-              child: Padding(
-                padding: const EdgeInsets.all(Dimens.large),
-                child: ElevatedButton(
-                  onPressed: () {},
-                  child: Text(
-                    "انتخاب مبدا",
-                    style: MyTextStyles.bottun,
-                  ),
-                ),
-              ),
-            ),
+            currentWidget(),
             MyBackButton(
-              onPressed: () {},
+              onPressed: () {
+                if (currentWidgetList.length > 1) {
+                  setState(() {
+                    currentWidgetList.removeLast();
+                  });
+                }
+              },
             ),
           ],
+        ),
+      ),
+    );
+  }
+
+  Widget currentWidget() {
+    Widget widget = orgin();
+
+    switch (currentWidgetList.last) {
+      case CurrentWidgetState.stateSelectOrgin:
+        widget = orgin();
+        break;
+      case CurrentWidgetState.stateSelectDestination:
+        widget = dest();
+        break;
+      case CurrentWidgetState.stateRequestDriver:
+        widget = reqDriver();
+        break;
+    }
+    return widget;
+  }
+
+  Positioned orgin() {
+    return Positioned(
+      bottom: 0,
+      left: 0,
+      right: 0,
+      child: Padding(
+        padding: const EdgeInsets.all(Dimens.large),
+        child: ElevatedButton(
+          onPressed: () {
+            setState(() {
+              currentWidgetList.add(CurrentWidgetState.stateSelectDestination);
+            });
+          },
+          child: Text(
+            "انتخاب مبدا",
+            style: MyTextStyles.bottun,
+          ),
+        ),
+      ),
+    );
+  }
+
+  Positioned dest() {
+    return Positioned(
+      bottom: 0,
+      left: 0,
+      right: 0,
+      child: Padding(
+        padding: const EdgeInsets.all(Dimens.large),
+        child: ElevatedButton(
+          onPressed: () {
+            setState(() {
+              currentWidgetList.add(CurrentWidgetState.stateRequestDriver);
+            });
+          },
+          child: Text(
+            "انتخاب مقصد",
+            style: MyTextStyles.bottun,
+          ),
+        ),
+      ),
+    );
+  }
+
+  Positioned reqDriver() {
+    return Positioned(
+      bottom: 0,
+      left: 0,
+      right: 0,
+      child: Padding(
+        padding: const EdgeInsets.all(Dimens.large),
+        child: ElevatedButton(
+          onPressed: () {},
+          child: Text(
+            "درخواست راننده",
+            style: MyTextStyles.bottun,
+          ),
         ),
       ),
     );
